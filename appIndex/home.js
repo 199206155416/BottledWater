5c7d9699be016433af1e7b9c137fcb961ad65fff
Your Namexuezhenxiang <xuezx_2014@163.com>
2018-09-10 20:37:10 +0800

    样式修改

--- appIndex/home.js
+++ appIndex/home.js
@@ -3,13 +3,22 @@ mui.init({
 	pullRefresh: {
 	    container: ".mui-content",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
 	    down : {
-			style: 'circle',//必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
-			color:' #2BD009', //可选，默认“#2BD009” 下拉刷新控件颜色
-			height: '50px',//可选,默认50px.下拉刷新控件的高度,
-			range: '100px', //可选 默认100px,控件可下拉拖拽的范围
-			offset: '0px', //可选 默认0px,下拉刷新控件的起始位置
-			auto: true,//可选,默认false.首次加载自动上拉刷新一次
-			callback: function(){} //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+			// style: 'circle',//必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
+			// color:' #2BD009', //可选，默认“#2BD009” 下拉刷新控件颜色
+			// height: '50px',//可选,默认50px.下拉刷新控件的高度,
+			// range: '100px', //可选 默认100px,控件可下拉拖拽的范围
+			// offset: '0px', //可选 默认0px,下拉刷新控件的起始位置
+			// auto: true,//可选,默认false.首次加载自动上拉刷新一次
+			callback: function(){ //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+				// 获取焦点图
+				getFocusImg();
+				// 获取商品列表
+				getGoodsList();
+
+				setTimeout(function(){
+					mui('.mui-content').pullRefresh().endPulldown();
+				}, 3000);
+			}
 	    }
   	}
 });
@@ -118,7 +127,7 @@ function getGoodsList(){
 
 			if(e.resCode == 0){
 				var result = e.result;
-
+				$("#goodsList").html("");
 				for(var i = 0, len = result.length; i < len; i++){
 					var activityImg = result[i].mallCategory.strImg;
 					var mallGoodsList = result[i].mallGoodsList;
