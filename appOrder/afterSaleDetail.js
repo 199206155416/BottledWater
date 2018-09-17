c344cc1b0431682b8c717944f241eb9da6faf2bc
963523818@qq.com <963523818@qq.com>
2018-09-18 00:54:05 +0800

    退款列表对接，撤销申请

--- appOrder/afterSaleDetail.js
+++ appOrder/afterSaleDetail.js
@@ -1,88 +1,124 @@
 var currentWebview;
-var strOrderId; // 订单id
-
+var refundItem; // 退款数据
+alert(mui);
 mui.init({
 	swipeBack: false
+	// pullRefresh: {
+	//     container: ".mui-content",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+	//     down : {
+	// 		style: 'circle',//必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
+	// 		color:' #2BD009', //可选，默认“#2BD009” 下拉刷新控件颜色
+	// 		height: '50px',//可选,默认50px.下拉刷新控件的高度,
+	// 		range: '100px', //可选 默认100px,控件可下拉拖拽的范围
+	// 		offset: '0px', //可选 默认0px,下拉刷新控件的起始位置
+	// 		auto: true,//可选,默认false.首次加载自动上拉刷新一次
+	// 		callback: function(){} //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+	//     }
+ //  	}
 });
 
 mui.plusReady(function() {
 	currentWebview = plus.webview.currentWebview();
 	
-	strOrderId = currentWebview.strOrderId;
+	//refundItem = currentWebview.refundItem;
 
-	// 获取订单列表
-	getOrderDetail();
+	// 获取退款详情
+	//getRefundDetail();
 	
 	// 绑定事件
-	bindEvnet();
+	//bindEvent();
 });
 
 function bindEvent(){
+	$(".recall-apply").click(function(){
+		doCloseRefund();
+	});
 	
+	$(".change-apply").click(function(){
+		openEdit();
+	});
 	
-};
+}
+
+function openEdit(){
+	var id=refundItem.id;
+	
+}
+
+function doCloseRefund(){
+	var id=refundItem.id;
+		$.ajax({
+		url: prefix + "/refund/closeRedund",
+		type: "POST",
+		data: {"id":id}, 
+		dataType: "json",
+		success: function(res){
+				ajaxLog(res);
+				var result=res.result;
+				if(res.resCode == 0){
+					mui.toast("撤销成功");
+					mui.back();
+				}else{
+				   mui.alert(result, '提示', function(e) {
+			        },"div");
+				}
+			}
+		});
+}
 
 /**
  * 获取订单列表
  * @author xuezhenxiang
  */
-function getOrderDetail(){
-	$.ajax({
-		url: prefix + "/order/detail/" + strOrderId,
-		type: 'GET',
-		dataType: "json",
-		success: function(res){
-			// 打印请求报错日志
-			ajaxLog(res);
-
-			if(res.resCode == 0){
-				var result = res.result; // 数据
-				
-				var strReceiptUserName = result.strReceiptUserName; // 收货人姓名
-				var strReceiptMobile = result.strReceiptMobile; // 收货人电话
-				var strLocation = result.strLocation; // 省市区
-				var strDetailAddress = result.strDetailAddress; // 详细地址
-				var totalPrice = result.totalPrice; // 总价
-				var factPrice = result.factPrice; // 实付价格
-				var dtPayTime = result.dtPayTime; // 支付时间
-				var bucketNum = result.bucketNum; // 桶数量
-				var bucketMoney = result.bucketMoney; // 桶价格
-				var mallOrderDetailList = result.mallOrderDetailList; // 商品列表
-
-				for(var i = 0, len = mallOrderDetailList.length; i < len; i++){
-					var lGoodsId = mallOrderDetailList[i].id; // 商品id
-					var strSkuName = mallOrderDetailList[i].strSkuName; // 商品名称
-					var skuPrice = mallOrderDetailList[i].skuPrice; // 商品价格
-					var strGoodsImg = mallOrderDetailList[i].strGoodsImg; // 商品图片
-					var count = mallOrderDetailList[i].count; // 商品数量
-					
-					var orderListTemp = $("#orderListTemp").html();
-
-					orderListTemp = orderListTemp.replace("#lOrderId#", lOrderId);
-					orderListTemp = orderListTemp.replace("#strOrderNum#", strOrderNum);
-					orderListTemp = orderListTemp.replace("#strStateName#", strStateName);
-
-					var orderList = $(htmlTemplate);
-					
-					;(function(orderList){
-						orderList.on("click", function(){
-							pushWebView({
-								webType: 'newWebview_First',
-								id: 'appOrder/orderDetail.html',
-								href: 'appOrder/orderDetail.html',
-								aniShow: getaniShow(),
-								title: "订单详情",
-								isBars: false,
-								barsIcon: '',
-								extendOptions: extendOptions
-							});
-						});
-					})(orderList);
-
-					
-				}
-			}
-		}
-	})
-};
+function getRefundDetail(){
+		            var item=refundItem;
+					var strStateName=item.strStateName;
+					var refundReason=item.refundReason;
+					var refundMoney=item.refundMoney;
+					var remarks=item.remarks;
+					var createDate=item.createDate;
+					var state=item.state;
+					var strImgPath=item.strImgPath;
+					$('#refundReason').html(refundReason);
+					if(refundMoney&&0!=refundMoney){
+						$('#refundMoney').html("￥"+"refundMoney");
+					}else{
+						$('#refundMoney').parent().hide();
+					}
+					var goodsState=item.goodsState;
+					if(goodsState&&""!==goodsState){
+						$("#goodsState").html(goodsState);
+					}else{
+						$("#goodsState").parent().hide();
+					}
+					$('#remarks').html(remarks);
+					$('#createDate').html(createDate);
+					if(strImgPath){
+						$("#strImgPath").attr("src",strImgPath);
+					}
+					if(state!=0){
+						$(".order-state-title").html(strStateName);
+						$(".order-state-introduce").hide();
+					}
+					var order=item.mallOrder;
+				   var mallOrderDetailList=order.mallOrderDetailList;
+					for(var i1 = 0, len1 = mallOrderDetailList.length; i1 < len1; i1++){
+						var itemGoods=mallOrderDetailList[i1];
+						var id = itemGoods.id;
+						var strGoodsName = itemGoods.strSkuName;
+						var strGoodsImg = itemGoods.strGoodsImg;
+						var strGoodsSKUDetail = itemGoods.remarks;
+						var skuPrice = itemGoods.skuPrice;
+						var count = itemGoods.count;
+						var goodsTemplate = $("#goodsTemplate").html();
+						goodsTemplate = goodsTemplate.replace("#strGoodsImg#", strGoodsImg);
+						goodsTemplate = goodsTemplate.replace("#strGoodsTitle#", commonNameSubstr(strGoodsName, 34));
+						goodsTemplate = goodsTemplate.replace("#strGoodsSKUDetail#", commonNameSubstr(strGoodsSKUDetail, 28));
+						goodsTemplate = goodsTemplate.replace("#skuPrice#", skuPrice);
+						goodsTemplate = goodsTemplate.replace("#count#", count);
+						var goods = $(goodsTemplate);
+						$(".goods-list").append(goods);
+					}
+	
+}
 
