57795bfa814fcd93fe0c8806572b08f896569293
Your Namexuezhenxiang <xuezx_2014@163.com>
2018-09-23 13:17:44 +0800

    首页加载进度

--- appIndex/home.js
+++ appIndex/home.js
@@ -8,7 +8,7 @@ mui.init({
 			// height: '50px',//可选,默认50px.下拉刷新控件的高度,
 			// range: '100px', //可选 默认100px,控件可下拉拖拽的范围
 			// offset: '0px', //可选 默认0px,下拉刷新控件的起始位置
-			// auto: true,//可选,默认false.首次加载自动上拉刷新一次
+			auto: true,//可选,默认false.首次加载自动上拉刷新一次
 			callback: function(){ //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
 				// 获取焦点图
 				getFocusImg();
