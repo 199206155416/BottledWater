da6674635ec049c4c890866c2e3a892bb9a15ead
Your Namexuezhenxiang <xuezx_2014@163.com>
2018-09-26 04:54:23 +0800

    首页轮播图/购物车/个人中心

--- appIndex/home.js
+++ appIndex/home.js
@@ -1,26 +1,26 @@
 mui.init({
 	swipeBack: false,
 	pullRefresh: {
-	    container: ".mui-content",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
-	    down : {
+		container: ".mui-content", //下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+		down: {
 			// style: 'circle',//必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
 			// color:' #2BD009', //可选，默认“#2BD009” 下拉刷新控件颜色
 			// height: '50px',//可选,默认50px.下拉刷新控件的高度,
 			// range: '100px', //可选 默认100px,控件可下拉拖拽的范围
 			// offset: '0px', //可选 默认0px,下拉刷新控件的起始位置
-			auto: true,//可选,默认false.首次加载自动上拉刷新一次
-			callback: function(){ //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+			auto: true, //可选,默认false.首次加载自动上拉刷新一次
+			callback: function() { //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
 				// 获取焦点图
 				getFocusImg();
 				// 获取商品列表
 				getGoodsList();
 
-				setTimeout(function(){
+				setTimeout(function() {
 					mui('.mui-content').pullRefresh().endPulldown();
 				}, 3000);
 			}
-	    }
-  	}
+		}
+	}
 });
 var currentWebview;
 var marqueeArray = []; //跑马灯数据数组
@@ -38,21 +38,21 @@ mui.plusReady(function() {
 /**
  * 获取焦点图
  */
-function getFocusImg(){
+function getFocusImg() {
 	$.ajax({
 		url: prefix + "/sys/getTopImgs",
 		type: 'GET',
 		dataType: "json",
-		success: function(e){
+		success: function(e) {
 			// 打印请求报错日志
 			ajaxLog(e);
 
-			if(e.resCode == 0){
+			if(e.resCode == 0) {
 				var result = e.result;
 				var marqueeArray = [];
 
-				for(var i = 0, len = result.length; i < len; i++){
-					var marqueeItem  = {};
+				for(var i = 0, len = result.length; i < len; i++) {
+					var marqueeItem = {};
 					marqueeItem.contentId = result[i].strLink;
 					marqueeItem.imagerpath = result[i].strImg;
 					marqueeArray.push(marqueeItem);
@@ -64,85 +64,110 @@ function getFocusImg(){
 	});
 };
 
+///**
+// * 设置轮播图
+// * @author xuezhenxiang
+// */
+//function setMarquee(marqueeArray) {
+//	var sliderMarquee = document.getElementById('slider');
+//	var sliderGroup = document.createElement('div');
+//	sliderGroup.className = 'mui-slider-group mui-slider-loop';
+//	sliderMarquee.appendChild(sliderGroup);
+//	var sliderIndicator = document.createElement('div');
+//	sliderIndicator.className = 'mui-slider-indicator';
+//	sliderMarquee.appendChild(sliderIndicator);
+//	for (var i = 0; i < marqueeArray.length; i++) {
+//		if (0 == i) {
+//			var sliderItemDuplicate = document.createElement('div');
+//			sliderItemDuplicate.className = 'mui-slider-item mui-slider-item-duplicate';
+//			sliderItemDuplicate.innerHTML = '<a href="' + marqueeArray[marqueeArray.length - 1].contentId + '">\
+//					<img src="' + marqueeArray[marqueeArray.length - 1].imagerpath + '" />\
+//				</a>';
+//			sliderGroup.appendChild(sliderItemDuplicate);
+//		}
+//		var sliderItem = document.createElement('div');
+//		sliderItem.className = 'mui-slider-item';
+//		sliderItem.innerHTML = '<a href="' + marqueeArray[i].contentId + '">\
+//				<img src="' + marqueeArray[i].imagerpath + '" />\
+//			</a>';
+//		sliderGroup.appendChild(sliderItem);
+//		var indicatorItme = document.createElement('div');
+//		if (i == 0) {
+//			indicatorItme.className = 'mui-indicator mui-active';
+//		} else {
+//			indicatorItme.className = 'mui-indicator';
+//		}
+//		sliderIndicator.appendChild(indicatorItme);
+//		if (marqueeArray.length - 1 == i) {
+//			var sliderItemDuplicate = document.createElement('div');
+//			sliderItemDuplicate.className = 'mui-slider-item mui-slider-item-duplicate';
+//			sliderItemDuplicate.innerHTML = '<a href="' + marqueeArray[0].contentId + '">\
+//					<img src="' + marqueeArray[0].imagerpath + '" />\
+//				</a>';
+//			sliderGroup.appendChild(sliderItemDuplicate);
+//		}
+//		slider = mui('.mui-slider').slider({
+//			interval:2000
+//		});
+//	}
+//};
+
 /**
  * 设置轮播图
  * @author xuezhenxiang
  */
 function setMarquee(marqueeArray) {
-	var sliderMarquee = document.getElementById('slider');
-	var sliderGroup = document.createElement('div');
-	sliderGroup.className = 'mui-slider-group mui-slider-loop';
-	sliderMarquee.appendChild(sliderGroup);
-	var sliderIndicator = document.createElement('div');
-	sliderIndicator.className = 'mui-slider-indicator';
-	sliderMarquee.appendChild(sliderIndicator);
-	for (var i = 0; i < marqueeArray.length; i++) {
-		if (0 == i) {
-			var sliderItemDuplicate = document.createElement('div');
-			sliderItemDuplicate.className = 'mui-slider-item mui-slider-item-duplicate';
-			sliderItemDuplicate.innerHTML = '<a href="' + marqueeArray[marqueeArray.length - 1].contentId + '">\
-					<img src="' + marqueeArray[marqueeArray.length - 1].imagerpath + '" />\
-				</a>';
-			sliderGroup.appendChild(sliderItemDuplicate);
-		}
-		var sliderItem = document.createElement('div');
-		sliderItem.className = 'mui-slider-item';
-		sliderItem.innerHTML = '<a href="' + marqueeArray[i].contentId + '">\
+	var strHtml = "";
+	for(var i = 0; i < marqueeArray.length; i++) {
+		if(marqueeArray[i].contentId){
+			strHtml += '<div class="swiper-slide"><a href="' + marqueeArray[i].contentId + '">\
 				<img src="' + marqueeArray[i].imagerpath + '" />\
-			</a>';
-		sliderGroup.appendChild(sliderItem);
-		var indicatorItme = document.createElement('div');
-		if (i == 0) {
-			indicatorItme.className = 'mui-indicator mui-active';
-		} else {
-			indicatorItme.className = 'mui-indicator';
+			</a></div>'
+		}else{
+			strHtml += '<div class="swiper-slide">\
+				<img src="' + marqueeArray[i].imagerpath + '" />\
+			</div>'
 		}
-		sliderIndicator.appendChild(indicatorItme);
-		if (marqueeArray.length - 1 == i) {
-			var sliderItemDuplicate = document.createElement('div');
-			sliderItemDuplicate.className = 'mui-slider-item mui-slider-item-duplicate';
-			sliderItemDuplicate.innerHTML = '<a href="' + marqueeArray[0].contentId + '">\
-					<img src="' + marqueeArray[0].imagerpath + '" />\
-				</a>';
-			sliderGroup.appendChild(sliderItemDuplicate);
-		}
-		slider = mui('.mui-slider').slider({
-			interval:2000
-		});
+		
 	}
+	$("#banner").html(strHtml);
+	slider = new Swiper(".swiper-container", {
+		autoplay: 5000,
+		loop: true,
+		pagination: '.swiper-pagination',
+	});
 };
 
 /**
  * 获取商品列表
  * @author xuezhenxiang
  */
-function getGoodsList(){
+function getGoodsList() {
 	$.ajax({
 		url: prefix + "/goods/getModularGoods/0",
 		type: 'GET',
 		dataType: "json",
-		success: function(e){
+		success: function(e) {
 			// 打印请求报错日志
 			ajaxLog(e);
 
-			if(e.resCode == 0){
+			if(e.resCode == 0) {
 				var result = e.result;
 				$("#goodsList").html("");
-				for(var i = 0, len = result.length; i < len; i++){
+				for(var i = 0, len = result.length; i < len; i++) {
 					var activityImg = result[i].strImg;
 					var mallGoodsList = result[i].mallGoodsList;
 					var htmlTemplate = $("#goodsListTemplate").html();
 
-					if(activityImg){
-						htmlTemplate = htmlTemplate.replace("#activityImg#", "<img src='"+ activityImg +"' />");
-					}else{
+					if(activityImg) {
+						htmlTemplate = htmlTemplate.replace("#activityImg#", "<img src='" + activityImg + "' />");
+					} else {
 						htmlTemplate = htmlTemplate.replace("#activityImg#", "");
 					}
-					
 
 					var goodslist = $(htmlTemplate);
 
-					for(var i1 = 0, len1 = mallGoodsList.length; i1 < len1; i1++){
+					for(var i1 = 0, len1 = mallGoodsList.length; i1 < len1; i1++) {
 						var id = mallGoodsList[i1].id;
 						var strGoodsName = mallGoodsList[i1].strGoodsName;
 						var strMainImg = mallGoodsList[i1].strMainImg;
@@ -156,9 +181,9 @@ function getGoodsList(){
 						goodsTemplate = goodsTemplate.replace("#goodsSlogn#", commonNameSubstr(goodsSlogn, 28));
 						goodsTemplate = goodsTemplate.replace("#defaultSkuPrice#", defaultSkuPrice);
 
-						var goods = $(goodsTemplate);
-						;(function(){
-							goods.on("click", function(){
+						var goods = $(goodsTemplate);;
+						(function() {
+							goods.on("click", function() {
 								var goodsId = this.getAttribute('id');
 								var extendOptions = {
 									goodsId: goodsId
@@ -177,11 +202,10 @@ function getGoodsList(){
 						})();
 						goodslist.find(".goods-list").append(goods);
 					}
-					
+
 					$("#goodsList").append(goodslist);
 				}
 			}
 		}
 	})
 };
-
--- myCenter/mine.js
+++ myCenter/mine.js
@@ -1,5 +1,24 @@
 mui.init({
-	swipeBack: false
+	swipeBack: false,
+	pullRefresh: {
+	    container: ".container",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+	    down : {
+			// style: 'circle',//必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
+			// color:' #2BD009', //可选，默认“#2BD009” 下拉刷新控件颜色
+			// height: '50px',//可选,默认50px.下拉刷新控件的高度,
+			// range: '100px', //可选 默认100px,控件可下拉拖拽的范围
+			// offset: '0px', //可选 默认0px,下拉刷新控件的起始位置
+			// auto: true,//可选,默认false.首次加载自动上拉刷新一次
+			callback: function(){ //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+				// 获取余额
+				getBalace();
+
+//				setTimeout(function(){
+//					mui('.container').pullRefresh().endPulldown();
+//				}, 3000);
+			}
+	    }
+  	}
 });
 var parentWebView;
 var touxiangimg;
@@ -67,6 +86,7 @@ function getBalace(){
 		type: "GET",
 		dataType: "json",
 		success: function(res){
+			mui('.container').pullRefresh().endPulldown();
 			ajaxLog(res);
 			if(res.resCode == 0){
 				var result = res.result;
